3411eab3c1945a94507ebade3355df6bfa988de6
Buwaneka Saranga <Buwaneka.Saranga@ukg.com>
2023-09-01 21:34:15 +0530

    Tests for the new features and refactored code

--- PayloadInjectionFilter_Tests/HelperTests.cs
+++ PayloadInjectionFilter_Tests/HelperTests.cs
@@ -1,4 +1,9 @@
-﻿using PayloadInjectionFilter_Tests.CustomTypes;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
+using PayloadInjectionFilter_Tests.CustomTypes;
 using Zone24x7PayloadExtensionFilter.HelperExtensions;
 
 namespace PayloadInjectionFilter_Tests
@@ -6,6 +11,7 @@ namespace PayloadInjectionFilter_Tests
     [TestFixture]
     internal class HelperTests
     {
+
         [Test (Description = "Determines that the type of an object is string")]
         public void DeterminesStringTypeFromObjectType()
         {
@@ -60,6 +66,54 @@ namespace PayloadInjectionFilter_Tests
 
             Assert.That(userId.IsString(), Is.False);
             Assert.That(userCode.IsString(), Is.True);
+        }
+
+        [Test(Description = "Determines an enumerable type")]
+        public void DeterminesAnObjectTypeIsAnEnumerable()
+        {
+            var list = new List<int>
+            {
+                1, 2, 3,
+            };
+
+            int[] list2 = new int[] { 1, 2, 3 };
+
+            HashSet<int> list3 = new HashSet<int> 
+            {
+                1, 2, 3,
+            };
+
+            Assert.True(list.GetType().IsEnumerable());
+            Assert.True(list2.GetType().IsEnumerable());
+            Assert.True(list3.GetType().IsEnumerable());
+        }
+
+        [Test(Description = "A string should not pass out as enumerable")]
+        public void AStringShouldNotPassAsAnEnumerable()
+        {
+            string someString = "this is a nice string and not really an enumerable in this context";
+            Assert.IsFalse(someString.GetType().IsEnumerable());
+        }
+
+        [Test(Description = "Returns true if the action executing context is of the expected HTTP Verb")]
+        public void ReturnsTrueIfTheActionExecutingContextIsOfExpectedHttpVerb()
+        {
+            var httpContext = new DefaultHttpContext();
+            httpContext.Request.Method = "POST";
+
+            var actionExecContext = new ActionExecutingContext(
+                new ActionContext(
+                    httpContext, 
+                    new RouteData(), 
+                    new ActionDescriptor()
+                ), 
+                new List<IFilterMetadata>{ }, 
+                new Dictionary<string, object> { }, 
+                null
+            );
+
+            Assert.IsTrue(actionExecContext.IsOneOfAllowedHttpMethods(new string[] { "POST", "PUT", "PATCH" }));
+            Assert.IsFalse(actionExecContext.IsOneOfAllowedHttpMethods(new string[] { "OPTIONS", "HEAD", "DELETE" }));
         }
     }
 }
