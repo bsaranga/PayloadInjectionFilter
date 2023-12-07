2f3f313b2d7a64929989d6d4a1778a9091c0fe7a
Buwaneka Saranga <Buwaneka.Saranga@ukg.com>
2023-12-07 14:25:25 +0530

    Test models and Test API

---
+++ TestModels/MultiValuedModel.cs
@@ -0,0 +1,16 @@
+﻿namespace TestModels
+{
+    public class MultiValuedModel
+    {
+        public MultiValuedModel(string Value1, string Value2, string Value3)
+        {
+            this.Value1 = Value1;
+            this.Value2 = Value2;
+            this.Value3 = Value3;
+        }
+
+        public string Value1 { get; }
+        public string Value2 { get; }
+        public string Value3 { get; }
+    }
+}
